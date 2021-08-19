d9e16885f39eecb3f14be51d52c7fbf2e52c6c22
Nick Stockton <phoenix76728@gmail.com>
2021-08-19 15:17:15 -0400

    inital commit

---
+++ Variables Excercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variables_Excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = "James";
+            int age = 36;
+            char lastInital = 'G';
+            bool maratial_Status = false;
+            double workHours = 46.5;
+            decimal hourlyPay = 8.55m;
+
+            Console.WriteLine($"My name is {name} {lastInital}. Am I married? {maratial_Status}. I work for  {workHours} hours a week at {hourlyPay} dollars an hour.");
+
+
+        }
+    }
+}
